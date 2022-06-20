b67e8d52dd8a1f965827fec7a4d21481e3e62758
Peter Seibel <peter@gigamonkeys.com>
2022-06-20 13:34:37 -0400

    Add typing input to repl div with contenteditable span.

--- web.js
+++ web.js
@@ -1,36 +1,79 @@
-window.frames[0].window.outputLine = (text) => {
+const input = document.getElementById("input");
+const output = document.getElementById("output");
+const cursor = document.getElementById("cursor");
+const prompt = document.getElementById("prompt");
+
+const newPrompt = () => {
   const div = document.createElement("div");
-  div.innerText = text;
-  document.getElementById("output").append(div);
+  div.append(prompt);
+  div.append(cursor);
+  output.append(div);
+  cursor.focus();
 };
 
-window.frames[0].window.onerror = (message, source, line, column, error) => {
+/*
+ * Output a line in the output div.
+ */
+const outputLine = (text) => {
+  const div = document.createElement("div");
+  div.innerText = "" + text;
+  output.append(div);
+  newPrompt();
+};
+
+/*
+ * Show errors from evaluating code.
+ */
+const showError = (message, source, line, column, error) => {
   const div = document.createElement("div");
   div.classList.add("error");
-  div.innerText = error;
-  document.getElementById("output").append(div);
+  div.innerText = "Error: " + error;
+  output.append(div);
+  newPrompt();
+  return true;
+};
+
+/*
+ * Send code to the iframe to be added as a script tag. The script tag can wrap
+ * the code in calls to outputLine to send output back to our #output div.
+ */
+const evaluate = (code) => {
+  try {
+    const d = window.frames[0].document;
+    const s = d.createElement("script");
+    s.append(document.createTextNode(code));
+    d.documentElement.append(s);
+  } catch (e) {}
+};
+
+submit.onclick = (e) => {
+  const text = input.value;
+  evaluate(`${text}; outputLine('ok.');`);
   return false;
 };
 
-// Send code to the iframe to be added as a script tag. The script tag can wrap
-// the code in calls to outputLine to send output back to our #output div.
-const evaluate = (code) => {
-  const d = window.frames[0].document;
-  const s = d.createElement("script");
-  console.log("script code: " + code);
-  s.innerText = code;
-  d.documentElement.append(s);
-};
-
-document.getElementById("input").onkeypress = (e) => {
+cursor.onkeypress = (e) => {
   if (e.key === "Enter") {
-    const text = e.target.value;
-    console.log(text);
-    outputLine("> " + text);
-    if (text.startsWith("let") || text.startsWith("const") || text.startsWith("var")) {
-      evaluate(`${text}; outputLine(void 0)`);
-    } else {
-      evaluate(`outputLine(${text})`);
-    }
+    const text = cursor.innerText;
+    const parent = cursor.parentNode;
+    const p = prompt.cloneNode(true);
+    p.removeAttribute("id");
+    parent.replaceChild(p, prompt);
+    parent.insertBefore(document.createTextNode(text), cursor);
+    cursor.replaceChildren();
+    parent.removeChild(cursor);
+    evaluate(`outputLine(${text})`);
+    return false;
   }
 };
+
+window.frames[0].window.outputLine = outputLine;
+window.frames[0].window.onerror = showError;
+cursor.onerror = showError;
+
+output.onfocus = (e) => {
+  console.log("here");
+  cursor.focus();
+};
+
+cursor.focus();
