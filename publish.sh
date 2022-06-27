a7ee5bfa5cd108dce8f366a17ba841bb4d88dcf6
Peter Seibel <peter@gigamonkeys.com>
2022-06-26 17:37:19 -0700

    Update publish.sh

--- publish.sh
+++ publish.sh
@@ -7,9 +7,12 @@ dir=$(basename "$(pwd)")
 sha=$(git log --pretty=tformat:%H -1);
 webdir=~/web/www.gigamonkeys.com/misc/$dir/
 
+if [ -d "$webdir" ]; then
+    rm -rf "$webdir";
+fi
 mkdir -p "$webdir"
 cp -R "$@" $webdir
 cd $webdir
-git add .
+git add -A .
 git commit -m "Publish $dir $sha" .
 git push
